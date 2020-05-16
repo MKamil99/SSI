e8a7a62201bd13d5a3930a68cf3c232cc7fdb1d2
MKamil99 <43967269+MKamil99@users.noreply.github.com>
2020-05-16 11:45:15 +0200

    Added exam

---
+++ Kolokwium/Kolokwium/NeuralNetwork/Functions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kolokwium.NeuralNetwork
+{
+    class Functions
+    {
+        private static double Alpha { get; set; } = 0.5;
+
+        // Funkcja licząca błąd średniokwadratowy - używana do celów testowych:
+        public static double CalculateError(List<double> outputs, int row, double[][] expectedresults)
+        {
+            double error = 0;
+            for (int i = 0; i < outputs.Count; i++)
+                error += Math.Pow(outputs[i] - expectedresults[row][i], 2);
+            return error;
+        }
+
+        // Funkcja wejściowa: iloczyn wartości wyjściowych wszystkich synaps wejściowych tego neuronu
+        public static double InputSumFunction(List<Synapse> Inputs)
+        {
+            double input = 0;
+            foreach (Synapse syn in Inputs)
+                input += syn.GetOutput();
+            return input;
+        }
+
+        public static double BipolarLinearFunction(double input) // funkcja aktywacji: bipolarna liniowa
+            => (1 - Math.Pow(Math.E, -Alpha * input)) / (1 + Math.Pow(Math.E, -Alpha * input));
+
+        public static double BipolarDifferential(double input)   // pochodna funkcji aktywacji (bipolarnej liniowej)
+            => (2 * Alpha * Math.Pow(Math.E, -Alpha * input)) / (Math.Pow(1 + Math.Pow(Math.E, -Alpha * input), 2));
+    }
+}
---
+++ Kolokwium/Kolokwium/NeuralNetwork/Layer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Kolokwium.NeuralNetwork
+{
+    class Layer
+    {
+        public List<Neuron> Neurons;  // lista neuronów wchodzących w skład warstwy sieci
+
+        // Tworzy listę "pustych" neuronów, które stanowią razem jedną warstwę sieci neuronowej:
+        public Layer(int numberofneurons)
+        {
+            Neurons = new List<Neuron>();
+            for (int i = 0; i < numberofneurons; i++)
+                Neurons.Add(new Neuron());
+        }
+
+        // Łączy dwie warstwy ze sobą poprzez dopinanie do każdego neuronu warstwy bieżącej 
+        // każdego neuronu warstwy następnej
+        public void ConnectLayers(Layer outputlayer)
+        {
+            foreach (Neuron thisneuron in Neurons)
+                foreach (Neuron thatneuron in outputlayer.Neurons)
+                    thisneuron.AddOutputNeuron(thatneuron);
+        }
+
+        // Dla każdego neuronu w warstwie wylicza jego wartość wejściową i wyjściową; zapobiega to konieczności
+        // wielokrotnego przechodzenia przez całą sieć w celu wyznaczenia poszczególnych wartości - zwłaszcza podczas
+        // nauczania algorytmem wstecznej propagacji
+        public void CalculateOutputOnLayer()
+        {
+            foreach (Neuron neuron in Neurons)
+                neuron.CalculateOutput();
+        }
+    }
+}
